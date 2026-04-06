923dca1f35c67f2d054f0680207fbfc032f77c89
newest-j <ibukunagbaoye@gmail.com>
2026-04-06 15:32:32 +0100

    sectionheader completed

---
+++ client/src/components/SectionHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-between items-center">
+    <div>
+      <h1 class="font-bold text-2xl sm:text-4xl text-text1">{{ title }}</h1>
+      <p class="text-text2">{{ description }}</p>
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string
+  description: string
+}
+defineProps<Props>()
+</script>
+
+<style scoped></style>
