6bc7bc3f92f7c4a41a01c713a57f96d024edc750
newest-j <ibukunagbaoye@gmail.com>
2026-04-24 17:18:35 +0100

    dashboarinfocard completed

---
+++ client/src/components/instructor/DashboardInfoCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="border border-background rounded-md p-2 flex justify-between w-full items-center">
+    <div>
+      <div class="flex items-center gap-2">
+        <h1 class="font-bold text-lg text-text1">{{ title }}</h1>
+        <span>{{ mode }}</span>
+      </div>
+      <p class="text-background">{{ coursestudents }} Students enrolled</p>
+    </div>
+    <div>
+      <div class="flex gap-2 text-text2"></div>
+      <EyeIcon class="size-3" />
+      <PencilSquareIcon class="size-3" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PencilSquareIcon, EyeIcon } from '@heroicons/vue/24/outline'
+interface Props {
+  title: string
+  mode: string
+  coursestudents: number
+}
+defineProps<Props>()
+</script>
+
+<style scoped></style>
